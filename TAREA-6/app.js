3d7192f166514f7b181740ec6d56312c9fa3fc43
ajalbur <ajalbur001@gmail.com>
2024-10-02 21:04:12 -0600

    tarea6

---
+++ TAREA-6/app.js
@@ -0,0 +1,22 @@
+function ver(num) {
+    y=1;
+    let x1="x\n";
+    while(y!=num) {
+    x1=x1+"x\n";
+    y++;
+    }
+    return x1;
+    }
+    function hor (num) {
+    z=1;
+    let x2="x";
+    while(z!=num){
+    }
+    x2=x2+"x";
+    Z++;
+    return x2;
+    let num1-parseInt(prompt("Ingrese el numero de la longitud que quiera su linera vertical: "));
+    let num2-parseInt(prompt("Ingrese el numero de la longitud que quiera su linera horizontal: "));
+    console.log(ver (num1));
+    console.log(hor (num2));
+    
